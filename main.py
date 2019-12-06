c883635f00f3798a186fb12b5d44ac929babb1e3
hsu <viewsonic521@gmail.com>
2019-12-06 18:07:37 +0900

    update

--- main.py
+++ main.py
@@ -89,7 +89,8 @@ def handle_text_message(event):
 
     # slack側に投稿するメッセージの加工
     send_msg = " {user_name}說\n".format(user_name=user_name) \
-               + "{msg}\n".format(msg=event.message.text) 
+               + "{msg}\n".format(msg=event.message.text)  \
+               + "roomdId{room_id}"
 
     # メッセージの送信
     slack_info.notify(text=send_msg)
