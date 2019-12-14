3218546c461e1ea2af97eed8e32a793e93dd9f9e
hsu <viewsonic521@gmail.com>
2019-12-14 16:07:16 +0900

    update

--- main.py
+++ main.py
@@ -96,7 +96,8 @@ def get_event_info(event):
     """
 
     # LINEユーザー名の取得
-    wks.update_acell('A1', event.replyToken)
+    wks.update_acell('A1', event.replyToken.value)
+    print(event.replyToken.value)
     #line_bot_api.reply_message(event.replyToken, '123')
     user_id = event.source.user_id
     try:
